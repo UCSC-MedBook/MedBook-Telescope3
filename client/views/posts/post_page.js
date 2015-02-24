b27672b4214cbc77cee4144ea134aea27f2ea70f
Ted Goldstein <tedgoldstein@gmail.com>
2015-02-24 09:30:35 -0800

    Added next and previous buttons

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,5 +1,6 @@
 Template[getTemplate('post_page')].helpers({
   post_item: function () {
+    Session.set("current_id", this._id);
     return getTemplate('post_item');
   },
   post_body: function () {
@@ -13,8 +14,42 @@ Template[getTemplate('post_page')].helpers({
   }
 });
 
+function GoAdjacent(which) {
+   var map = Session.get(which);
+   var _id = Session.get("Post_id");
+   var url = null;
+   if (map && _id && _id in map) {
+       var adjacent_id = map[_id];
+       if (adjacent_id)  {
+           setTimeout(function() {
+               Router.go("post_page", {_id: adjacent_id});
+           },200);
+           return;
+       }
+   } 
+
+   var orig = Session.get("PostOriginalRoute");
+   if (orig)
+       url = orig;
+
+   if (url == null) url = "/";
+   setTimeout(function() {
+       Router.go(url);
+   },200);
+}
+
+
 Template[getTemplate('post_page')].rendered = function(){
   $('body').scrollTop(0);
   if(this.data) // XXX
     document.title = $(".post-title").text();
+
+  $('.Previous').click(function() {
+    GoAdjacent("Previous")
+  });
+  $('.Next').click(function() {
+    GoAdjacent("Next")
+  });
+       
 };
+
