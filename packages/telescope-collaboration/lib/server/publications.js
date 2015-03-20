1c0af4e94cc5ccec1d8057df7c5723c118c7ffc7
Ted Goldstein <tedgoldstein@gmail.com>
2015-03-19 23:39:30 -0700

    Bugfix for to make Unlisted collaborations  listable.

--- packages/telescope-collaboration/lib/server/publications.js
+++ packages/telescope-collaboration/lib/server/publications.js
@@ -7,18 +7,25 @@ Meteor.publish('collaboration', function() {
       var collaborations = user.profile.collaborations;
       var emails = getEmailsForId(this.userId);
 
+
+
       if (collaborations == null)
           return Collaboration.find( {isUnlisted: false} );
       else
           return Collaboration.find(
               {$or: [
                   {isUnlisted: false}, // allows people to join
-                  {$and: [
-                      {isUnlisted: true}, // here to show the true branch
-                      {collaborators: {$in: user.profile.collaborations}}
+                  {$and:
+                      [
+                          {isUnlisted: true}, // here to show the true branch
+                          {$or: [
+                              {collaborators: {$in: user.profile.collaborations}},
+                              {collaborators: {$in: emails}},
+                              {administrators: {$in: user.profile.collaborations}},
+                              {administrators: {$in: emails}}
+                          ]}
                       ]
                   },
-                  {administrators: emails}
               ]});
 });
 
