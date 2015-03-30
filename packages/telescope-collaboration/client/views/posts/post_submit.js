7b6d94523bba52f63377823acac06acb6a12fee3
Ted Goldstein <tedgoldstein@gmail.com>
2015-03-30 00:21:52 -0700

    Begun to present posted svgs.

--- packages/telescope-collaboration/client/views/posts/post_submit.js
+++ packages/telescope-collaboration/client/views/posts/post_submit.js
@@ -27,5 +27,19 @@ Meteor.startup( function() {
             }
           },
         });
+    Template[getTemplate('post_submit')].hooks( {
+        rendered: function() {
+            var posted_svg = parent.getSvg();
+            console.log("posted_svg", posted_svg);
+            if (posted_svg) {
+                $('#postedContent').html("<div style='border:1px solid black;width:800;height:400px'> " 
+                + '<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="100%" height="100%" viewBox="0 0 1100 600" id="svg2" version="1.1" inkscape:version="0.48.0 r9654">'
+
+
+
+                    + posted_svg + "</svg> <div>");
+            }
+        }
+    });
     Template[getTemplate('post_submit-medbook')].replaces(getTemplate('post_submit'));
 });
