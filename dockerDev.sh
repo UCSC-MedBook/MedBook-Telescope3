eb780b266928a220c3b77e881db88d6038f38d05
Mike Risse <rissem@gmail.com>
2016-01-23 13:44:55 -0800

    fix to work with compose --x-networking

--- dockerDev.sh
+++ dockerDev.sh
@@ -2,5 +2,5 @@
 
 cd /app-dev
 cp --no-clobber -r /app/* /app-dev
-export MONGO_URL=mongodb://mongo:${MONGO_PORT_27017_TCP_PORT}/MedBook
+export MONGO_URL=mongodb://mongo:27017/MedBook
 meteor --release $RELEASE
